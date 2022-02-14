50c0a4fe607c7ee106a2a5b42e02cc00f3d6d636
Joe Zakielarz <joseph.zakielarz@duke.edu>
2022-02-14 12:42:39 -0500

    Ford wheel overlay

--- test/fordraptoroverlay.py
+++ test/fordraptoroverlay.py
@@ -3,7 +3,7 @@
 from PIL import Image
 
 wheel = Image.open('transparentwheel.png').convert("RGBA")
-chassis = Image.open('chassis.png').convert("RGBA")
+chassis = Image.open('fordraptorchassis.png').convert("RGBA")
 
 
 width = (chassis.width - wheel.width) // 2
@@ -16,12 +16,12 @@ wpercent = (basewidth/float(wheel.size[0]))
 hsize = int((float(wheel.size[1])*float(wpercent)))
 wheel = wheel.resize((basewidth,hsize), Image.ANTIALIAS)
 
-width1 = chassis.width//3 - 45
-width2 = 2 * chassis.width//3 + 30
-height = 575
+width1 = chassis.width//3 - 60
+width2 = 2 * chassis.width//3 + 25
+height = 540
 # Paste the frontImage at (width, height)
 chassis.paste(wheel, (width1, height), wheel)
 chassis.paste(wheel, (width2, height), wheel)
 
 # Save this image
-chassis.save("new.png", format="png")
+chassis.save("newfordraptor.png", format="png")
