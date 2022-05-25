bc1a891e6a274509c914f0a1298d54166e4ac500
Joe Zakielarz <joseph.zakielarz@duke.edu>
2022-05-25 15:42:02 -0400

    Add Season and Component structs

--- contracts/JoeComponent1155.sol
+++ contracts/JoeComponent1155.sol
@@ -25,47 +25,34 @@ contract XLR8Components is ERC1155, Ownable {
     using Strings for uint256;
 
     address public FullCarContract; // So we can approve access for all
-    uint256 private _lastSeasonTotalNumComponents; // Use this to determine if an id is in the newest season
 
     string public name;
     string public symbol;   
-    string _prerevealURI; 
     uint256 public currentSeason;
 
     struct Season {
         bool revealed;
         uint256 maxMintPerAddress;
-        uint256 totalNumComponents;
+        uint256 mintingFee;
+        uint256 lastSeasonTotalNumComponents; // Use this to determine if an id is in the newest season
         string baseUri;
         string prerevealUri;
     }
 
-    // Mapping of the season # to the Season struct with its data
-    mapping(uint256 => Season) public seasonToSeasonData;
+    struct Component {
+        uint8 componentType; // 0: model, 1: wrap, 2: engine
+        uint8 season;
+        uint256 maxSupply;
+        uint256 numberMinted;
+    }
 
+    mapping(uint256 => Season) public seasonToSeasonData; // Season # to the Season struct data
+    mapping(uint256 => mapping(address => uint256)) public seasonToAddressToNumberOfMints; // Season # to address to # of times they've minted in that season
+    mapping(uint256 => Component) public idToComponent;
+    
 
-    /* 
-        Minting variables
-    */
-    uint256 public maxMintForSeason;
-    address private _lastAddressToMint; // Address of last person who minted to factor into the mint randomness function
-
-    // TODO: Somehow combine these mappings into a struct? Will this save space?
-    // Like a Season struct with all its data!! Do this!! // TODO
-
-    mapping(uint256 => bool) public seasonToRevealed;
-
-    // Mapping of component ID to the type: 0: model, 1: wrap, 2: engine
-    mapping(uint256 => uint256) public idToComponentType;
-
-    // Mapping of component id => max supply of that id
-    mapping(uint256 => uint256) public idToMaxSupply;
-
-    // Mapping of the season to that season's base URI 
-    mapping(uint256 => string) public seasonToBaseURI;
-
-    // Mapping of the token Id to its season (for uri reasons)
-    mapping(uint256 => uint256) public idToSeason;
+    // Address of last person who minted to factor into the mint randomness function
+    address private _lastAddressToMint; 
 
     // Mapping of season to the timestamp of the reveal
     // mapping (uint256 => uint256) public seasonToRevealTimestamp;
@@ -77,7 +64,8 @@ contract XLR8Components is ERC1155, Ownable {
         string memory _season0prerevealuri,
         uint256[] memory _maxSupplies, 
         uint256[] memory _componentTypes,
-        uint256 _maxMintForSeason0
+        uint256 _maxMintForSeason0,
+        uint256 _season0mintingFee
     ) ERC1155(_season0baseuri) {
         require(_maxSupplies.length == _componentTypes.length, "_maxSupplies and _componentTypes must be the same length");
 
@@ -85,25 +73,17 @@ contract XLR8Components is ERC1155, Ownable {
         symbol = _symbol;
 
         currentSeason = 0;
-        maxMintForSeason = _maxMintForSeason0;
-        seasonToRevealed[currentSeason] = false;
-        seasonToBaseURI[currentSeason] = _season0baseuri;
+        seasonToSeasonData[currentSeason] = Season(false, _maxMintForSeason0, _season0mintingFee, 0, _season0baseuri, _season0prerevealuri);
 
         _lastAddressToMint = msg.sender; // Arbitrarily chose this, not really important
 
         // Same code as addNewComponents, but wanted function parameter to be calldata,
         // and not compatible with constructor memory paramter
-        _lastSeasonTotalNumComponents = _tokenIds.current();
-        seasonToRevealed[currentSeason] = false; 
-        _prerevealURI = _season0prerevealuri;
-
         uint256 tokenId;
         for (uint i = 0; i < _maxSupplies.length; i++) {
             tokenId = _tokenIds.current();
-            idToMaxSupply[tokenId] = _maxSupplies[i];
-            idToComponentType[tokenId] = _componentTypes[i];
-
-            idToSeason[tokenId] = currentSeason;
+            // Creates a new component and puts it in the mapping
+            idToComponent[tokenId] = Component(_componentTypes[i], currentSeason, _maxSupplies[i], 0);
 
             require(_maxSupplies[i] > 0, "Max supply cannot be negative");
             require(_componentTypes[i] >= 0 && _componentTypes[i] <= 2, "Invalid component types");
@@ -125,23 +105,20 @@ contract XLR8Components is ERC1155, Ownable {
         uint256[] calldata _componentTypes,
         string calldata _seasonBaseUri,
         string calldata _newPrerevealUri ,
-        uint256 _maxMintForSeason ) public onlyOwner {
+        uint256 _maxMintForSeason,
+        uint256 _mintingFee ) public onlyOwner {
         require(_maxSupplies.length == _componentTypes.length, "_maxSupplies and _componentTypes must be the same length");
-        maxMintForSeason = _maxMintForSeason;
 
-        _lastSeasonTotalNumComponents = _tokenIds.current();
         currentSeason += 1;
-        seasonToRevealed[currentSeason] = false; 
-        seasonToBaseURI[currentSeason] = _seasonBaseUri;
-        _prerevealURI = _newPrerevealUri;
+        uint256 lastSeasonTotalNumComponents = _tokenIds.current();
+
+        seasonToSeasonData[currentSeason] = Season(false, _maxMintForSeason, _mintingFee, lastSeasonTotalNumComponents, _seasonBaseUri, _newPrerevealUri);
 
         uint256 tokenId;
         for (uint i = 0; i < _maxSupplies.length; i++) {
             tokenId = _tokenIds.current();
-            idToMaxSupply[tokenId] = _maxSupplies[i];
-            idToComponentType[tokenId] = _componentTypes[i];
-
-            idToSeason[tokenId] = currentSeason;
+            // Creates a new component and puts it in the mapping
+            idToComponent[tokenId] = Component(_componentTypes[i], currentSeason, _maxSupplies[i], 0);
 
             require(_maxSupplies[i] > 0, "Max supply cannot be negative");
             require(_componentTypes[i] >= 0 && _componentTypes[i] <= 2, "Invalid component types");
@@ -152,8 +129,8 @@ contract XLR8Components is ERC1155, Ownable {
     }
 
     function revealNewestSeason() public onlyOwner {
-        require(!seasonToRevealed[currentSeason], "This season's already been revealed!");
-        seasonToRevealed[currentSeason] = true;
+        require(!seasonToSeasonData[currentSeason].revealed, "This season's already been revealed!");
+        seasonToSeasonData[currentSeason].revealed = true;
     }
 
     // Component contract published before full car contract, so upload here
@@ -164,12 +141,13 @@ contract XLR8Components is ERC1155, Ownable {
     }
 
     function isInNewestSeason(uint256 _id) internal view returns (bool) {
-        return _id >= _lastSeasonTotalNumComponents;
+        return _id >= seasonToSeasonData[currentSeason].lastSeasonTotalNumComponents;
     }
 
+
     function getRandomNumber(uint256 _balanceOfNum, uint256 _previousRandNum) internal view returns (uint256) {
-        uint256 numInCurrentSeason = _tokenIds.current() - _lastSeasonTotalNumComponents;
-        return uint256(keccak256(abi.encodePacked(msg.sender, _lastAddressToMint, block.timestamp))) % numInCurrentSeason;
+        uint256 numInCurrentSeason = _tokenIds.current() - seasonToSeasonData[currentSeason].lastSeasonTotalNumComponents;
+        return uint256(keccak256(abi.encodePacked(_balanceOfNum, _previousRandNum, _lastAddressToMint, block.timestamp))) % numInCurrentSeason;
     }
 
     
@@ -179,10 +157,22 @@ contract XLR8Components is ERC1155, Ownable {
     // Set the _lastAddressToMint = msg.sender
     function mintComponent(uint256 amount) public payable {
         require(tx.origin == msg.sender);
-        require(msg.value >= mintingFee, "The value submitted is less than the minting fee");
+        require(msg.value >= amount * seasonToSeasonData[currentSeason].mintingFee, "Incorrect payment");
 
-        uint256 randnum = getRandomNumber();
-        _mint(msg.sender, randnum, 1, "");
+        seasonToAddressToNumberOfMints[currentSeason][msg.sender] = 0; // TODO: Maybe take this out, might init to 0 anyways
+
+        uint256 mintId = 0;
+        for (uint i = 0; i < amount; i++) {
+            uint balanceOfpreviousMintId = balanceOf(msg.sender, mintId);
+            mintId = getRandomNumber(balanceOfpreviousMintId, mintId);
+
+            // TODO: check if randNum's been minted out, and if so, increment until it settles in
+            
+
+            _mint(msg.sender, mintId, 1, "");
+            seasonToAddressToNumberOfMints[currentSeason][msg.sender] += 1;
+        }
+        
 
 
     }
@@ -192,7 +182,7 @@ contract XLR8Components is ERC1155, Ownable {
      * @dev Indicates whether any token exist with a given id, or not.
      */
     function exists(uint256 id) public view returns (bool) {
-        return idToMaxSupply[id] > 0;
+        return idToComponent[id].maxSupply > 0;
     }
 
     /**
@@ -204,10 +194,10 @@ contract XLR8Components is ERC1155, Ownable {
         require(exists(_id), "URI: nonexistent token");
 
         // If it's in the newest season and the reveal hasn't happened yet
-        if (isInNewestSeason(_id) && !seasonToRevealed[currentSeason]) {
-            return _prerevealURI;
+        if (isInNewestSeason(_id) && !seasonToSeasonData[currentSeason].revealed) {
+            return seasonToSeasonData[currentSeason].prerevealUri;
         }
 
-        return string(abi.encodePacked(seasonToBaseURI[idToSeason[_id]], Strings.toString(_id)));
+        return string(abi.encodePacked(seasonToSeasonData[idToComponent[_id].season].baseUri, Strings.toString(_id)));
     }
 }
